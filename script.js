c18b40978b2ae9774be0d41c18018d26de43f6e1
Vlad Vyalov <vyalov92@yandex.ru>
2022-02-13 01:31:23 +0500

    main

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-let container = document.querySelector('.page')
-let popupOpenButtonForm = document.querySelector('.profile__edit-button')
-let popupForm = document.querySelector('.popup')
-let popupCloseButtonForm = popupForm.querySelector('.popup__button_close')
-let profileTitle = document.querySelector('.profile__title')
-let profileSubtitle =  document.querySelector('.profile__subtitle')
-let nameInput = document.querySelector('.popup__profile_name')
-let jobInput = document.querySelector('.popup__profile_job')
+const container = document.querySelector('.page')
+const popupOpenButtonForm = document.querySelector('.profile__edit-button')
+const popupForm = document.querySelector('.popup')
+const popupCloseButtonForm = popupForm.querySelector('.popup__button_type_close')
+const profileTitle = document.querySelector('.profile__title')
+const profileSubtitle =  document.querySelector('.profile__subtitle')
+const nameInput = document.querySelector('.popup__input_type_name')
+const jobInput = document.querySelector('.popup__input_type_job')
 
 
-let openPopup = function() {
+const openPopup = function() {
   popupForm.classList.add('popup_open')
-  if (popupForm.classList.contains('popup_open')) {
-    nameInput.value = profileTitle.textContent;
-    jobInput.value = profileSubtitle.textContent;
-  }
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 }
 
-let closePopup = function() {
+const closePopup = function() {
     popupForm.classList.remove('popup_open')
 }
 
 popupOpenButtonForm.addEventListener('click', openPopup)
 popupCloseButtonForm.addEventListener('click', closePopup)
 
-function formSubmitHandler (evt) {
+function handleProfileFormSubmit (evt) {
     evt.preventDefault(); 
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
+    closePopup()
 }
 
-popupForm.addEventListener('submit', formSubmitHandler);
+popupForm.addEventListener('submit', handleProfileFormSubmit);
